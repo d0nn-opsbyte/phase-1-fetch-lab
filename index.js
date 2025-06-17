68e81a72a2bb5e1398bcbed6d7f2d415b78285f0
donald kiarie <donaldkiarie768@gmail.com>
2025-06-17 16:55:40 +0300

    Add file

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((books) => {
+      renderBooks(books);
+    
+   console.log("5th book:", books[4].name);
 
+  fetch("https://anapioficeandfire.com/api/characters/1031")
+    .then(res => res.json())
+    .then(character => console.log("1031st character:", character.name));
+
+  const totalPages = books.reduce((sum, book) => sum + book.numberOfPages, 0);
+  console.log("Total number of pages:", totalPages);
+
+   });
+
+}
+  
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
